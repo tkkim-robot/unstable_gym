89dfa7fb1161a02d698c912a326cc42dab498c49
Taekyung Kim <taekyung@umich.edu>
2025-04-11 03:40:05 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='unstable_gym',
     version='0.1.0',
-    packages=['unstable_gym'],
-    url='https://github.com/ktk1501/unstable_gym',
-    license='',
+    packages=find_packages(),
     author='Taekyung Kim',
     author_email='ktk1501@kakao.com',
     description='A gym-like classical control benchmark for evaluating the robustnesses of control and reinforcement learning algorithms.',
     install_requires=[
-        'gym',
+        'gymnasium',
         'numpy',
     ],
-    tests_require=[
-    ]
 )
